8288fd45c3ef6ce1111ad6e1ab76b913b9f3c40c
RAJARAJESHWARI R <rajarajeshwari803@gmail.com>
2018-08-13 11:30:51 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class set
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>0)
+        {
+
+        if(a%2==0)
+        {
+            System.out.print("even");
+        }
+        else 
+        {
+            System.out.print("odd");
+        }
+        }
+            System.out.println("invalid");
+        
+    }
+}
