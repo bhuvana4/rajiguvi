de2de30888907c2f11d2cd46b88465e69e818a49
RAJARAJESHWARI R <rajarajeshwari803@gmail.com>
2018-08-15 15:03:39 +0530

    Create sumint.java

---
+++ sumint.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class countn{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int c=0;
+        for(int i=0;i<=a;i++)
+        {
+            c=c+i;
+        }
+        if(c!=0)
+        {
+            System.out.print(c);
+        }
+    }
+}
