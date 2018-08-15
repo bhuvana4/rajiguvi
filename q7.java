7037f4583379f4464de349e9cd1670b2049ff232
RAJARAJESHWARI R <rajarajeshwari803@gmail.com>
2018-08-15 15:08:03 +0530

    Create q7.java

---
+++ q7.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class countn{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+     String a="Hello";
+     int b=sc.nextInt();
+     for(int i=1;i<=b;i++)
+     {
+         System.out.println(a);
+     }
+    }
+}
