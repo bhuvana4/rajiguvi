66a8abdba535c776fcd6ec2791d1a0d08bb9eeb2
RAJARAJESHWARI R <rajarajeshwari803@gmail.com>
2018-08-15 16:00:57 +0530

    Create q93.java

---
+++ q93.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class countn{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+     int a=sc.nextInt();
+     if(a>=0)
+     {
+         System.out.print(a+273);
+     }
+    }
+}
