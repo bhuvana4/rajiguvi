c97702447d9a82015516cc0a998d97b1d3087ce5
RAJARAJESHWARI R <rajarajeshwari803@gmail.com>
2018-08-15 15:49:49 +0530

    Create q105.java

---
+++ q105.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class countn{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+         String a=sc.next();
+        String b=sc.next();
+        System.out.print(a+""+b);
+    }
+}
